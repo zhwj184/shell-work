aa5fdf83494200027405e7f7f2766fa01bca6ba1
weijian.zhongwj <zhongweijian@ubuntu.(none)>
2013-04-22 23:10:58 +0800

    deleted:    cookielog/cookielog.sh 	deleted:    cookielog/cookielog.sh~ 	deleted:    mysql/mysqlmonitor.sh 	deleted:    mysql/mysqlmonitor.sh~ 	deleted:    net/net.sh 	deleted:    net/net.sh~

--- net/net.sh
+++
@@ -1,5 +0,0 @@
-#打印tcp连接信息
-netstat -tlnp
-
-#统计处于ESTABLISHED 连接状态的ip列表和连接数
-netstat -an | grep ESTABLISHED | awk '{print $5}' | awk -F: '{print $1}' | sort | uniq -c | awk '{ printf("%s\t%s\t",$2,$1) ; for (i = 0; i < $1; i++) {printf("*")}; print "" }'
